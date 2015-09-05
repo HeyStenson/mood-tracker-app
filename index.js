c541b5c3d27c01527f60fd904db7faecf9572080
HeyStenson <h.n.stenson@gmail.com>
2015-09-05 14:33:43 -0700

    can sign new user up and redirect to home page

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ var bodyParser = require('body-parser');
 //views path
 var views = path.join(process.cwd(), 'views/');
 var db = require('./models');
-
 // var session = require('express-session');
 
-// CONFIG //
 // serve js & css files
 app.use("/static", express.static("public"));
 app.use("/vendor", express.static("bower_components"));
@@ -16,8 +14,9 @@ app.use("/vendor", express.static("bower_components"));
 // body parser config to accept all datatypes
 app.use(bodyParser.urlencoded({extended: true}));
 
+// parse cookie data
 // var cookieParser = require("cookie-parser");
-// app.use(cookieParser()); // parse cookie data
+// app.use(cookieParser()); 
 
 //set the view engine to ejs
 //app.set('view engine', 'ejs');
@@ -35,6 +34,20 @@ app.get("/login", function(req, res){
 app.get("/signup", function(req, res){
   var signup_form = path.join(views, "signup.html");
   res.sendFile(signup_form);
+});
+
+//API endpoints
+
+app.post(["/signup", "/api/users"], function createUser(req, res){
+  console.log("Looks like you're trying to signup!");
+  var email = req.body.email;
+  var passwordDigest = req.body.passwordDigest;
+  db.User.create({email: email, passwordDigest: passwordDigest}, function(err, newUser){
+    if (err){return console.log(err);}
+    console.log(newUser + "is registered");
+    //res.cookie("guid", newUser._id, { signed: true });
+    res.redirect("/");
+  });
 });
 
 //listen up
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 var Day = new Schema({
 	morning: String,
@@ -9,11 +10,66 @@ var Day = new Schema({
 });
 
 var userSchema = new Schema({
-	email: {type: String, required: true},
-	passwordDigest: {type: String, required: true},
-	createdAt: {type: Date, default: Date.now()},
+	email: {
+		type: String, 
+		required: true, 
+		index: {
+			unique: true
+		}
+	},
+	passwordDigest: {
+		type: String, 
+		required: true
+	},
+	createdAt: {
+		type: Date, 
+		default: Date.now()},
 	days: [Day]
 });
+
+// create a new user with secure (hashed) password (for sign up)
+userSchema.statics.createSecure = function (email, password, cb) {
+  // `_this` now references our schema
+  var _this = this;
+  // generate some salt
+  bcrypt.genSalt(function (err, salt) {
+    // hash the password with the salt
+    bcrypt.hash(password, salt, function (err, hash) {
+      // build the user object
+      var user = {
+        email: email,
+        passwordDigest: hash
+      };
+      // create a new user in the db with hashed password and execute the callback when done
+      _this.create(user, cb);
+    });
+  });
+};
+
+// // authenticate user (for login)
+// userSchema.statics.authenticate = function (email, password, cb) {
+//   // find user by email entered at log in
+//   this.findOne({email: email}, function (err, user) {
+//     // throw error if can't find user
+//     if (user === null) {
+//       cb("Can\'t find user with that email", null);
+//     // if found user, check if password is correct
+//     } else if (user.checkPassword(password)) {
+//       // the user is found & password is correct, so execute callback
+//       // pass no error, just the user to the callback
+//       cb(null, user);
+//     } else {
+//       // user found, but password incorrect
+//       cb("Password incorrect", user)
+//     }
+//   });
+// };
+
+// // compare password user enters with hashed password (`passwordDigest`)
+// userSchema.methods.checkPassword = function (password) {
+//   // run hashing algorithm (with salt) on password to compare with stored `passwordDigest`
+//   return bcrypt.compareSync(password, this.passwordDigest);
+// };
 
 var Day = mongoose.model("Day", Day);
 var User = mongoose.model("User", userSchema);
