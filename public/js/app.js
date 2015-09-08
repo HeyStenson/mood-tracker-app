3696eeee14f56285659a2744d79088b276d773eb
HeyStenson <h.n.stenson@gmail.com>
2015-09-08 12:00:16 -0700

    added basic color picker function

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
 $(function(){
 	getHour();
+	getColor();
 });
 
 function getHour(){
 	var rightNow = moment().hour();
 	if (rightNow > 0 && rightNow < 12){
 		$('#time').append('<h1>Good morning!</h1>');
-	} else if (rightNow > 12 && rightNow < 6) {
+	} else if (rightNow >= 12 && rightNow < 17) {
 		$('#time').append('<h1>Good afternoon!</h1>');
-	} else {
+	} else if (rightNow >= 17) {
 		$('#time').append('<h1>Good evening!</h1>');
 	}
 };
+
+function getColor(){
+	$('div.mood').click(function(){
+		var moodNow = $(this).attr('id');
+		console.log(moodNow);
+	})
+}
